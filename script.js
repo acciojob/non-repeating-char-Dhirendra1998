b1c661fc241775716ddf6ba7f1890a6092b244af
Dhirendra Man Singh <132539583+Dhirendra1998@users.noreply.github.com>
2023-07-14 13:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let input =prompt("Enter a String")
+
+function f (str){
+	for(let i =0;i<str.length,i++){
+		const currchar = str[i];
+		if(str.indexOf(currchar)=== str.lastIndexOf(currchar)){
+			return currchar;
+		}
+	}
+	return null;
+}
+
+const f1 = f(input);
+
+if (f1) {
+  console.log(`The first non-repeated character is: ${firstNonRepeatedChar}`);
+} else {
+  console.log("There are no non-repeated characters in the string.");
+}
